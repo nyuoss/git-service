e8e47b7a785c12d1f87101d10cd4ca3268833904
SiqiWan <704252212@qq.com>
2024-04-21 14:27:59 -0400

    rename project-path to git-service and initialize go swagger

---
+++ git_functions/docs.go
@@ -0,0 +1,11 @@
+// Package git_service GitsService.
+//
+// Documentation of our awesome API.
+//
+//	Schemes: http
+//	BasePath: /v1/
+//	Version: 1.0.0
+//	Host: localhost:8000
+//
+// swagger:meta
+package git_service
