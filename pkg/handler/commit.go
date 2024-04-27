0de55a62072f2d30b12a3387d899ebd375e60052
PraneethKumarT <praneeth.kumt@gmail.com>
2024-04-27 15:02:21 -0400

    Get commit before

--- pkg/handler/commit.go
+++ pkg/handler/commit.go
@@ -1,8 +1,22 @@
 package handler
 
 import (
-	"net/http"
+    "encoding/json"
+    "fmt"
+	"io"
+    "net/http"
+
+    "github.com/gorilla/mux"
+    "github.com/go-resty/resty/v2"
 )
+type commit struct {
+    SHA     string `json:"sha"`
+    Commit  struct {
+        Author struct {
+            Date string `json:"date"`
+        } `json:"author"`
+    } `json:"commit"`
+}
 
 type CommitHandler interface {
 	GetCommitsBefore(http.ResponseWriter, *http.Request)
@@ -19,7 +33,50 @@ func NewCommitHandler() CommitHandler {
 }
 
 func (h *commitHandler) GetCommitsBefore(w http.ResponseWriter, r *http.Request) {
-	// TODO
+    w.Header().Set("Content-Type", "application/json")
+
+    vars := mux.Vars(r)
+    owner := vars["owner"]
+    repo := vars["repo"]
+    commitID := vars["commit"]
+
+    client := resty.New()
+    apiUrl := fmt.Sprintf("https://api.github.com/repos/%s/%s/commits?sha=%s", owner, repo, commitID)
+
+    resp, err := client.R().
+   
+        Get(apiUrl)
+
+    if err != nil {
+        http.Error(w, fmt.Sprintf("Failed to fetch data: %s", err.Error()), http.StatusInternalServerError)
+        return
+    }
+
+    if resp.StatusCode() != http.StatusOK {
+        body, _ := io.ReadAll(resp.RawResponse.Body)
+        http.Error(w, fmt.Sprintf("GitHub API returned status code: %d, body: %s", resp.StatusCode(), string(body)), resp.StatusCode())
+        return
+    }
+
+    var commits []commit
+    if err := json.Unmarshal(resp.Body(), &commits); err != nil {
+        http.Error(w, "Failed to parse commits data", http.StatusInternalServerError)
+        return
+    }
+
+    commitsByBranch := mapCommitsToBranches(commits)
+    if err := json.NewEncoder(w).Encode(commitsByBranch); err != nil {
+        http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+    }
+}
+
+func mapCommitsToBranches(commits []commit) map[string][]string {
+    result := make(map[string][]string)
+    for _, c := range commits {
+        branchName := "main"  // Placeholder: you would need to determine the branch somehow
+        result[branchName] = append(result[branchName], c.SHA)
+    }
+    return result
 }
 
 func (h *commitHandler) GetCommitsAfter(w http.ResponseWriter, r *http.Request) {
