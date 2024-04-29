51fa37f2d1ba53bf9ab1c27f5a7f8ec93d46330e
Aryaman Rishabh <aryamanrishabh@gmail.com>
2024-04-29 17:03:39 -0400

    get active branches API

--- pkg/handler/branch.go
+++ pkg/handler/branch.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 
@@ -24,7 +25,6 @@ func NewBranchHandler() BranchHandler {
 }
 
 func (h *branchHandler) GetActiveBranches(w http.ResponseWriter, r *http.Request) {
-	// TODO
 	vars := mux.Vars(r)
 	owner := vars["owner"]
 	repo := vars["repo"]
@@ -35,12 +35,73 @@ func (h *branchHandler) GetActiveBranches(w http.ResponseWriter, r *http.Request
 	unit := queryParams.Get("unit")
 	number := queryParams.Get("number")
 
-	branches := []string{owner, repo, unit, number}
-	resp := GetActiveBranchesResp{
-		Branches: branches,
+	duration, err := time.ParseDuration(number + unit)
+	if err != nil {
+		http.Error(w, "Invalid time unit", http.StatusBadRequest)
+		return
 	}
 
-	_ = json.NewEncoder(w).Encode(resp)
+	var repoBranches []Branch
+	client := resty.New()
+	startTime := time.Now().Add(-duration)
+
+	apiURL := fmt.Sprintf("https://api.github.com/repos/%s/%s/branches", owner, repo)
+	resp, err := client.R().SetResult(&repoBranches).Get(apiURL)
+
+	if err != nil {
+		http.Error(w, "Failed to fetch branches from the GitHub API", http.StatusInternalServerError)
+		return
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		http.Error(w, "GitHub API returned status code: %d", resp.StatusCode())
+		return
+	}
+
+	var activeBranches []string
+	for _, branch := range repoBranches {
+		commit, _ := getCommit(owner, repo, branch.Commit.SHA)
+		latestCommitDate := commit.Author.Date
+		date, err := time.Parse(time.RFC3339, latestCommitDate)
+
+		if err != nil {
+			http.Error(w, "Failed to decode datetime", http.StatusInternalServerError)
+			return
+		}
+
+		if date.After(startTime) {
+			activeBranches = append(activeBranches, branch.Name)
+		}
+	}
+
+	response := GetActiveBranchesResp{
+		Branches: activeBranches,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(response)
+}
+
+func getCommit(owner, repo, commitSHA string) (*Commit, error) {
+	var commit CommitDetails
+	client := resty.New()
+
+	resp, err := client.R().
+		SetResult(&commit).
+		Get(fmt.Sprintf("https://api.github.com/repos/%s/%s/commits/%s", owner, repo, commitSHA))
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		return nil, fmt.Errorf("GitHub API returned status code: %d", resp.StatusCode())
+	}
+
+	var commitInfo = commit.Commit
+
+	return &commitInfo, nil
 }
 
 func (h *branchHandler) GetBranchByTag(w http.ResponseWriter, r *http.Request) {
