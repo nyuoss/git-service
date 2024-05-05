a6333df6c7fbd404b4f1f05d34fe008af3a9cf6d
Aryaman Rishabh <aryamanrishabh@gmail.com>
2024-05-05 14:48:18 -0400

    add test case for  getActiveBranches

--- pkg/handler/branch_test.go
+++ pkg/handler/branch_test.go
@@ -37,3 +37,34 @@ func TestGetBranchByTag(t *testing.T) {
 		t.Errorf("handler returned unexpected body: got %v want %v", actualResponseBody, expectedResponseBody)
 	}
 }
+
+func TestGetActiveBranches(t *testing.T) {
+	req, err := http.NewRequest("GET", "/v1/aryamanrishabh/metricsjs/branch/getActiveBranches?unit=h&number=300", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := mux.NewRouter()
+	h := &branchHandler{}
+	r.HandleFunc("/v1/{owner}/{repo}/branch/getActiveBranches", h.GetActiveBranches).Methods("GET")
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned incorrect status code")
+	}
+
+	expectedContentType := "application/json"
+	if contentType := rr.Header().Get("Content-Type"); contentType != expectedContentType {
+		t.Errorf("Handler returned incorrect content type")
+	}
+
+	expected := `{"branches":["master"]}`
+	got := strings.TrimSpace(rr.Body.String())
+	
+	if got != expected {
+		t.Errorf("Handler returned unexpected body")
+	}
+}
+
