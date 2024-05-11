2b99a03c70724f310df8c2d146960f95bd66b8ca
Siddharth-Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2024-05-11 13:54:52 -0400

    fixed linting

--- pkg/handler/commit.go
+++ pkg/handler/commit.go
@@ -223,7 +223,7 @@ func (h *commitHandler) GetJobsByCommit(w http.ResponseWriter, r *http.Request) 
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(statusesJSON)
+	_ = json.NewEncoder(w).Encode(statusesJSON)
 }
 
 func GetCommitStatuses(owner, repo, commitSHA string) ([]Status, error) {
