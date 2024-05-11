9730501cd438bebef466d72edde47c4b8caf5ac8
Aryaman Rishabh <aryamanrishabh@gmail.com>
2024-05-11 14:36:20 -0400

    add test for commits after

--- pkg/handler/commit.go
+++ pkg/handler/commit.go
@@ -58,7 +58,7 @@ func (h *commitHandler) GetCommitsAfter(w http.ResponseWriter, r *http.Request) 
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	
+
 	commitDateTime := commit.Author.Date
 	datetime, err := time.Parse(time.RFC3339, commitDateTime)
 	if err != nil {
@@ -104,7 +104,6 @@ func (h *commitHandler) GetCommitsAfter(w http.ResponseWriter, r *http.Request) 
 				additions++
 			}
 
-			
 			if number != "" {
 				num, err := strconv.Atoi(number)
 				if err != nil {
